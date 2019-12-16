09a0177dc9e954f641380bc74ff94d509a0f1682
Alex Madeira <alex.c.madeira@gmail.com>
2019-12-16 19:52:48 -0300

    html parser

--- src/pages/Lore/index.js
+++ src/pages/Lore/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import ReactHtmlParser from 'react-html-parser';
 
+import { useChampion } from '~/services/hooks/Champion';
 import { Container } from './styles';
 
 export default function Lore() {
-  return <Container>Lore</Container>;
+  const { championMore } = useChampion();
+
+  return (
+    <Container>
+      {championMore && ReactHtmlParser(championMore.biography.full)}
+    </Container>
+  );
 }
